af584c78012dd949421f74c58fea4e8fdb4e1037
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-06-10 13:48:12 +0900

    styled-componets 전역 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router/Router';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  html {
+        font-size: 16px;
+    }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const theme = {
+  color: {
+    primary: '#FFB2B2',
+  },
+  size: {
+    sm: '20px',
+    md: '50px',
+    lg: '100px',
+  },
+};
 
 export default App;
