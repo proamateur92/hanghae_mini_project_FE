96683fe79aa9f91268ae22afa64143f723bb6aff
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-06-13 06:41:59 +0900

    Main 컴포넌트에서 게시글 뿌려주는 BoardList 컴포넌트 분리

--- src/Router/Main.js
+++ src/Router/Main.js
@@ -1,106 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Header from './Header';
-import { useNavigate } from 'react-router-dom';
-import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
-import { faPencil, faTrashCan, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+import BoardList from '../components/BoardList';
+import { faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { useNavigate } from 'react-router-dom';
 const Main = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [isMore, setIsMore] = useState(false);
-  const [isComment, setIsComment] = useState(false);
   const boards = useSelector(state => state.board.list);
-  const comments = useSelector(state => state.comment.list);
 
-  // 각 게시글 별 댓글 가져오기
-  const commentBox = articleId => {
-    const filteredComment = comments.filter(comment => comment.articleId === articleId);
-    return filteredComment.map(comment => (
-      <>
-        <div>
-          {comment.nickName} {comment.comment}
-        </div>
-      </>
-    ));
-  };
-
-  // 각 게시글 별 댓글 갯수 값 계산 로직
-  const onComment = board => {
-    const filteredComment = comments.filter(comment => comment.articleId === board.articleId);
-    return <Comment onClick={() => setIsComment(prev => !prev)}>댓글 {filteredComment.length}개</Comment>;
-  };
   return (
     <>
       <Header />
       <Container>
         <Box>
-          <List>
-            {boards &&
-              boards.map(board => (
-                <Item key={board.articleId}>
-                  <Text>
-                    <Top>
-                      <Nickname>{board.nickName}</Nickname>
-                      <Edit>
-                        <Icon>
-                          <FontAwesomeIcon
-                            onClick={e => {
-                              e.stopPropagation();
-                              navigate(`/write/${board.articleId}`, { state: board });
-                            }}
-                            icon={faPencil}
-                            size='lg'
-                          />
-                        </Icon>
-                        <Icon>
-                          <FontAwesomeIcon icon={faTrashCan} size='lg' />
-                        </Icon>
-                      </Edit>
-                    </Top>
-                    {isMore ? (
-                      <>
-                        <Content>
-                          {board.content}
-                          <More onClick={() => setIsMore(false)}>접기</More>
-                        </Content>
-                      </>
-                    ) : (
-                      <>
-                        {board.content.length > 30 ? (
-                          <Content>
-                            {board.content.slice(0, 60) + '...'}
-                            <More onClick={() => setIsMore(true)}>더보기</More>
-                          </Content>
-                        ) : (
-                          <Content>{board.content}</Content>
-                        )}
-                      </>
-                    )}
-                  </Text>
-                  <ImageBox>
-                    <Image src={board.imageURL} />
-                  </ImageBox>
-                  <Detail>
-                    <Like>
-                      <FontAwesomeIcon icon={faThumbsUp} size='lg' />
-                      <Count>2</Count>
-                    </Like>
-                    {onComment(board)}
-                  </Detail>
-                  {isComment && (
-                    <>
-                      <span>닉네임</span>
-                      <input type='text' />
-                      {commentBox(board.articleId)}
-                    </>
-                  )}
-                  {!isComment && null}
-                </Item>
-              ))}
-          </List>
+          <List>{boards && boards.map(board => <BoardList board={board} />)}</List>
         </Box>
       </Container>
       <WriteButton>
@@ -109,17 +23,6 @@ const Main = () => {
     </>
   );
 };
-
-const WriteButton = styled.div`
-  position: fixed;
-  right: 20px;
-  bottom: 20px;
-  cursor: pointer;
-  transition: 0.4s;
-  &:hover {
-    color: #076be1;
-  }
-`;
 
 const Container = styled.div`
   position: relative;
@@ -131,97 +34,24 @@ const Container = styled.div`
 const List = styled.div``;
 const Box = styled.div`
   padding: 20px;
+  /* @media (min-width: 499px) {
+    width: 90%;
+    max-width: 440px;
+  }
+  @media (min-width: 500px) {
+    width: 90%;
+    max-width: 50px;
+  } */
 `;
 
-const Item = styled.div`
-  margin: 100px 0;
-  border-radius: 10px;
-  overflow: hidden;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+const WriteButton = styled.div`
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  cursor: pointer;
   transition: 0.4s;
-`;
-
-const Icon = styled.div``;
-const Top = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 15px;
-  ${Icon} {
-    margin-left: 15px;
-  }
-  ${Icon}:hover {
-    transform: scale(1.1);
-  }
-  ${Icon}:first-child:hover {
-    color: #076be1;
-  }
-  ${Icon}:last-child:hover {
-    color: red;
-  }
-`;
-
-const Edit = styled.div`
-  display: flex;
-`;
-
-const Nickname = styled.span``;
-const Content = styled.span``;
-const More = styled.span``;
-const Text = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 30px 20px;
-  ${Nickname} {
-    color: #9c9c9c;
-    display: block;
-    font-weight: bold;
-    font-size: 16px;
-  }
-  ${Content} {
-    font-size: 16px;
-  }
-  ${More} {
-    color: #383838;
-    font-weight: bold;
-    cursor: pointer;
-  }
-`;
-
-const Image = styled.img``;
-const ImageBox = styled.div`
-  width: 100%;
-  font-size: 0;
-  ${Image} {
-    width: 100%;
-  }
-`;
-
-const Comment = styled.span``;
-const Detail = styled.div`
-  margin: 0 10px;
-  padding: 15px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 20px;
-  ${Comment} {
-    color: #383838;
-    cursor: pointer;
-    font-size: 16px;
-  }
-`;
-
-const Count = styled.span``;
-const Like = styled.div`
-  color: #000;
   &:hover {
     color: #076be1;
-    transform: scale(1.1);
-    font-weight: bold;
-  }
-  ${Count} {
-    margin-left: 5px;
   }
 `;
 
