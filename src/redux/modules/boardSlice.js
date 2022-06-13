7c2836eeb2bfe9d6dfc95f6f81159492a4ab9c54
jeelly <khk3159@naver.com>
2022-06-13 10:35:49 +0900

    3장 까지만 첨부가능 문구 추가, boardSlice에 미들웨어 생성, reducers update기능 구현(글, 사진 불러옴 정렬 포함) 이미치첨부 사진 바꾸기, 테두리 제거 후 박스 쉐도우 추가, 새로고침 누를 때 오류 뜨는 거 수정, 게시물 uuid 사용해서 고유 id 랜덤 생성, 게시글 임시 닉네임 추가 #8 #10

--- src/redux/modules/boardSlice.js
+++ src/redux/modules/boardSlice.js
@@ -1,4 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+//미들웨어
+
+  //Create
+  export const createBoardDB = (contents_obj) => {
+    return async function (dispatch) {
+      await axios.post("http://localhost:5000/boards", contents_obj)
+        .then((response) => {  
+        })
+        .catch(function(error) {
+          console.log("에러",error.response.data);
+        })
+        .then(function () {
+          // 항상 실행되는 영역
+        });
+      dispatch(createBoard(contents_obj));
+    };
+  };
+
+  //Update
+  export const updateBoardDB = (contents_obj, id) => {
+    return async function (dispatch) {
+      // await axios
+      //     .patch("http://localhost:5000/boards", contents_obj, id)
+      //     .then((response) => {
+      //       console.log("aa",response)
+      //     })
+      //     .catch(function(error) {
+      //       // console.log("에러",error.response.data);
+      //       console.log("에러",error);
+      //     });
+      dispatch(updateBoard(contents_obj, id));
+    };
+  };
 
 const boardSlice = createSlice({
   name: "board",
@@ -20,7 +56,13 @@ const boardSlice = createSlice({
       state.list.push(action.payload);
     },
     updateBoard(state, action) {
-      console.log("리듀서 수정", action.payload);
+      const newState = state.list.filter((l, idx) => {
+        return l.id !== parseInt(action.payload.id);
+      });
+      const newwState = [...newState, action.payload];
+      state.list = newwState.sort(function(a,b) {
+        return a.id - b.id;
+      });
     },
     reamoveBoard(state, action) {},
   },
