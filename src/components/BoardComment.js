a28e94bea3effc0945153ac7c35389cb2af16fdb
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-06-13 11:38:25 +0900

    댓글 수정, 삭제 활성화 레이아웃 반영(기능x)

--- src/components/BoardComment.js
+++ src/components/BoardComment.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const BoardComment = ({ board }) => {
   const [comment, setComment] = useState('');
+  const [option, setOption] = useState(false);
 
   // 각 게시글 별 댓글 가져오기
   const comments = useSelector(state => state.comment.list);
@@ -13,6 +16,7 @@ const BoardComment = ({ board }) => {
   };
 
   const submitHandler = event => {
+    if (comment.trim() === '') return;
     if (event.type === 'submit') {
       event.preventDefault();
     }
@@ -23,42 +27,58 @@ const BoardComment = ({ board }) => {
   const commentBox = articleId => {
     const filteredComment = comments.filter(comment => comment.articleId === articleId);
     return filteredComment.map(comment => (
-      <CommentInput>
+      <CommentBox key={comment.id}>
         <span>{comment.nickName}</span>
-        <span>{comment.comment}</span>
-      </CommentInput>
+        <div>{comment.comment}</div>
+        <Icon onClick={() => setOption(prev => !prev)}>
+          <FontAwesomeIcon icon={faEllipsis} size='sm' />
+        </Icon>
+        {option && (
+          <>
+            <span>수정</span>
+            <span>삭제</span>
+          </>
+        )}
+        {!option && null}
+      </CommentBox>
     ));
   };
 
   return (
-    <CommentBox onSubmit={submitHandler}>
-      <>
-        <span>{board.nickName}</span>
+    <Container>
+      <InputBox onSubmit={submitHandler}>
         <input value={comment} onChange={commentHandler} type='text' placeholder='댓글을 입력하세요' />
         <Post mode={comment ? '' : 'off'} onClick={submitHandler}>
           게시
         </Post>
-      </>
+      </InputBox>
       {commentBox(board.articleId)}
-    </CommentBox>
+    </Container>
   );
 };
 
-const CommentBox = styled.form`
-  display: flex;
-  justify-content: space-around;
+const Container = styled.div`
   padding: 20px;
+`;
+
+const InputBox = styled.form`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
   span {
     font-weight: bold;
+    font-size: 14px;
   }
   input {
-    width: 60%;
+    width: 70%;
     outline: none;
     border: none;
     border-bottom: 1px solid #ccc;
     margin: 0 20px;
     padding-bottom: 2px;
     font-size: 16px;
+    text-align: center;
+    transition: 0.4s;
   }
   input:focus {
     border-bottom: 1.5px solid #bfdffb;
@@ -72,8 +92,31 @@ const Post = styled.span`
   cursor: pointer;
 `;
 
-const CommentInput = styled.div`
-  background-color: green;
+const Icon = styled.div``;
+const CommentBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 20px 0px;
+  span {
+    font-size: 14px;
+    font-weight: bold;
+  }
+  div {
+    margin-left: 15px;
+    background-color: #f0f2f5;
+    padding: 10px 15px;
+    border-radius: 10px;
+  }
+  ${Icon} {
+    margin-left: 10px;
+    padding: 2px 5px;
+    border-radius: 10px;
+    background-color: #fff;
+    cursor: pointer;
+  }
+  ${Icon}:hover {
+    background-color: #f0f2f5;
+  }
 `;
 
 export default BoardComment;
