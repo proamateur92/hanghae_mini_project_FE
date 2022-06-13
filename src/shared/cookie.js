c1142e2d80d68d50c7cc9a7ac58968da7eae6669
junsu0121 <sayber123@naver.com>
2022-06-14 03:30:28 +0900

    로그아웃시 토큰 삭제 구현 #9

--- src/shared/cookie.js
+++ src/shared/cookie.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Cookies } from "react-cookie";
 
 const cookies = new Cookies();
@@ -11,22 +12,3 @@ export const setCookie = (name, value, option) => {
 export const getCookie = (name) => {
   return cookies.get(name);
 };
-
-// export const removeCookie = (name) => {
-//   return localStorage.removeItem((name = "is_login"));
-// };
-
-export const removeCookie = () => {
-  return cookies.remove;
-};
-
-// export const removeCookie = (name) => {
-//   document.cookie = name + "=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
-// };
-
-// export const resetCookie = (cName) => {
-//   var expireDate = new Date();
-//   expireDate.setDate(expireDate.getDate() - 1);
-//   document.cookie =
-//     cName + "= " + "; expires=" + expireDate.toGMTString() + "; path=/";
-// };
