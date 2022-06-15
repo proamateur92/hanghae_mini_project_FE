b1d8e3362582576fba2cfcc80efc72e97ed049d3
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-06-15 12:25:51 +0900

    댓글 crud 기능 구현 - slice 소스 수정 #62

--- src/redux/modules/commentSlice.js
+++ src/redux/modules/commentSlice.js
@@ -1,18 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// 댓글 가져오기
-export const loadCommentDB = () => {
-  return async function (dispatch) {
-    try {
-      const response = await axios.get(`http://13.209.64.124/comment/`);
-      dispatch(loadComment(response.data.comment));
-    } catch (error) {
-      // 멘토링 때 물어보기
-    }
-  };
-};
-
 // 댓글 추가
 export const createCommentDB = targetId => {
   return async function (dispatch) {
