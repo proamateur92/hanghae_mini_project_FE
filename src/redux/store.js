d76a5b7bf9e3183ee15ce1e0aa061924ac226d54
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-06-15 21:23:50 +0900

    comment slice 파일 삭제

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import boardReducer from './modules/boardSlice';
 import userReducer from './modules/userSlice';
-import commentReducer from './modules/commentSlice';
 import likeReducer from './modules/likeSlice';
 
 const store = configureStore({
   reducer: {
     board: boardReducer,
     user: userReducer,
-    comment: commentReducer,
     like: likeReducer,
   },
 });
